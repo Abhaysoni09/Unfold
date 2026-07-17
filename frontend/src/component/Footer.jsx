6ae99eb21af5572ccae3f5a0a9aee818e405168b
Abhaysoni096 <abhaylkw332@gmail.com>
2026-07-17 21:44:23 +0530

    Fix footer social media icons

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -1,7 +1,7 @@
 import {
-  Instagram,
-  Facebook,
-  Twitter,
+  Camera,
+Users,
+MessageCircle,
   Mail,
   Phone,
   MapPin,
@@ -49,13 +49,13 @@ const Footer = () => {
             </p>
             <div className="flex gap-3 mt-6">
               <a className="w-10 h-10 rounded-full bg-[#163832] flex items-center justify-center hover:bg-[#235347] transition">
-                <Instagram size={18}/>
+                <Camera size={18}/>
               </a>
               <a className="w-10 h-10 rounded-full bg-[#163832] flex items-center justify-center hover:bg-[#235347] transition">
-                <Facebook size={18}/>
+                <Users size={18}/>
               </a>
               <a className="w-10 h-10 rounded-full bg-[#163832] flex items-center justify-center hover:bg-[#235347] transition">
-                <Twitter size={18}/>
+                <MessageCircle size={18}/>
               </a>
             </div>
           </div>
